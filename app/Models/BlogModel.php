fcf2b6c722da6492bcc571b4b51fafb5f731847a
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-01-20 14:25:16 -0300

    feat: model connect ok

--- app/Models/BlogModel.php
+++ app/Models/BlogModel.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-class BlogModel
+class BlogModel extends Conn 
 {
+    private object $conn;
+
     public function list()
     {
-        echo "Pesquisar no BD";
+        $this->conn = $this->connect();
+        $query_articles = "SELECT id, title, content FROM articles ORDER BY id DESC LIMIT 40";
+
+        $result_articles = $this->conn->prepare($query_articles);
+        $result_articles->execute();
+        $return = $result_articles->fetchAll();
+
+        return $return;
     }
 }
