a25b547f6c64f4bbb9299b6754b3bd966bc5c2ad
Guilhermefariah <Guilhermefaria321@gmail.com>
2025-01-20 11:00:30 -0300

    feat: ConfigController method construct for url default

--- core/ConfigController.php
+++ core/ConfigController.php
@@ -1,11 +1,31 @@
 <?php
+
 namespace Core;
 
-class ConfigController 
+class ConfigController
 {
+    private string $url;
+    private array $urlSet;
+    private string $urlController;
+    private string $urlMethod;
+
     public function __construct()
     {
-        echo filter_input(INPUT_GET, "url", FILTER_DEFAULT);
+        if (!empty(filter_input(INPUT_GET, "url", FILTER_DEFAULT))) {
+            $this->url = filter_input(INPUT_GET, "url", FILTER_DEFAULT);
+            $this->urlSet = explode("/", $this->url);
 
+            if ((isset($this->urlSet[0])) and (isset($this->urlSet[1]))) {
+                $this->urlController = $this->urlSet[0];
+                $this->urlMethod = $this->urlSet[1];
+            } else {
+                $this->urlController = "error";
+                $this->urlMethod = "index";
+            }
+        } else {
+            $this->urlController = "home";
+            $this->urlMethod = "index";
+        }
+        echo "Controller: {$this->urlController} - Método: {$this->urlMethod}";
     }
 }
